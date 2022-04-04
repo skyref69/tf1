8d7b34d5c77710734d5d0f95c4c6f217dc19803b
dD7&ee8(112rr <francesco.re.nttdata@skytv.it>
2022-04-04 09:01:48 +0200

    fix: Optimitation startStepFunction

--- src/LambdaStartStepFunction/index.ts
+++ src/LambdaStartStepFunction/index.ts
@@ -7,23 +7,33 @@ let stepfunctions = new AWS.StepFunctions()
 export const LambdaStartStepFunction = async(event: stateVoteDb) => {
     const id = event.Records[0].dynamodb.NewImage.id.S
     const poolStatus = event.Records[0].dynamodb.NewImage.poolStatus.S    
-    const taskToken = event.Records[0].dynamodb.NewImage.taskToken.S
-    try {      
-        if(poolStatus === 'opened' && taskToken === '0'){    
-            console.log('Command opened vote received');                       
-          let params = {
-            stateMachineArn: process.env.STATE_MACHINE_ARN,            
-            input: JSON.stringify({"dataOpened": {"id": id, "poolStatus": poolStatus}}),
-        }        
-        await stepfunctions.startExecution(params).promise();        
-        }else if(poolStatus == 'closed'){
-            console.log('Command closed vote received');
-            let taskToken = event.Records[0].dynamodb.NewImage.taskToken.S          
-            let params = {
-                output: '{}',
-                taskToken: taskToken            }               
-            await stepfunctions.sendTaskSuccess(params).promise();            
+    const taskToken = event.Records[0].dynamodb.NewImage.taskToken.S 
+    try {        
+        switch (poolStatus) {
+            case 'opened':  
+                console.log('Command opened vote received !');              
+                if (taskToken === '0') {
+                    let params = {
+                        stateMachineArn: process.env.STATE_MACHINE_ARN,            
+                        input: JSON.stringify({"dataOpened": {"id": id, "poolStatus": poolStatus}}),
+                    }        
+                    await stepfunctions.startExecution(params).promise(); 
+                }                
+                break;
+
+            case 'closed':
+                console.log('Command closed vote received !');                         
+                let params = {
+                    output: '{}',
+                    taskToken: taskToken            
+                }               
+                await stepfunctions.sendTaskSuccess(params).promise();  
+                break;
+        
+            default:
+                break;
         }
+        
     } catch (error) {
         console.log(error)        
     }    
